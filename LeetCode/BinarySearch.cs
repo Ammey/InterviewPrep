c87568ff2e1730c220045551ea82d726949eaf95
Ammey Naik Aroskar <ammey.naik@gmail.com>
2019-01-18 12:54:39 -0800

    binary search

--- LeetCode/BinarySearch.cs
+++ LeetCode/BinarySearch.cs
@@ -1,12 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace LeetCode
 {
-    class BinarySearch
+    public class BinarySearch
     {
+        public static int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Count() - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int Sqrt(int x)
+        {
+            if (x == 0)
+                return 0;
+            int left = 1;
+            int right = x;
+            int ans = 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (mid <= x / mid)
+                {
+                    left = mid + 1;
+                    ans = mid;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return ans;
+        }
+
+        public static void Main()
+        {
+            var nums = new int[] { -1, 0, 3, 5, 9, 12, 14 };
+            int target = -2;
+            var result = Search(nums, target);
+        }
     }
 }
